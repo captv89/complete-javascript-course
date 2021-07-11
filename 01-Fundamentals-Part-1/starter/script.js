6186965af22dd0dac5888fa59c2ae0a50653b98b
captv89 <techsavvysailor@gmail.com>
2021-07-11 13:41:05 +0530

    Assignment: Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,18 @@
 let js = 'Amazing';
-if (js === 'Amazing') alert('JS is FUN!');
+// if (js === 'Amazing') alert('JS is FUN!');
 console.log(40 + 20 - 30 * 12 / 23);
+
+// Lecture: Values and Variables
+let firstName = "John";
+let lastName = "Doe";
+
+var country, continent, population;
+
+country = 'India';
+continent = 'Asia';
+population = '136.64 crores';
+
+console.log("I am", firstName, lastName);
+console.log("Country: ", country);
+console.log("Continent: ", continent);
+console.log("Population: ", population);
